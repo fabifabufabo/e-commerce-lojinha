6bf4ebf24239f90c12f9bd5a8cee6b1df8c94a7e
Fábio Fernandes <dev.fabiofernandes@gmail.com>
2024-11-10 14:15:19 -0300

    feat: integrate MongoDB connection and update environment configuration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import routes from "./routes/index.js";
+import MongoDB from "./config/mongoDB.js";
 
 const app = express();
 routes(app);
+
+MongoDB.init();
 
 export default app;
--- src/config/environment.js
+++ src/config/environment.js
@@ -1,3 +1,7 @@
 export default {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  mongodb: {
+    url:
+      process.env.MONGODB_CONNECTION_STRING,
+  },
 };
